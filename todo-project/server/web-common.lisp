8fb86e57d5f67a4b62b644181d9b801a4d45da66
John Hilts <johnhilts@live.com>
2020-11-21 00:22:38 -0800

    todo webapp - experimental test macro (successful!)

--- todo-project/server/web-common.lisp
+++ todo-project/server/web-common.lisp
@@ -13,3 +13,14 @@
   `(defun ,name (raw-data)
      (let ((model (convert-dotted-pair-to-plist (json:decode-json-from-string raw-data))))
        ,@body)))
+
+(defmacro experiment-define-data-update-handler (name model &body body)
+  (let ((model-name (car model)))
+    `(defun ,name (raw-data)
+       (let ((,model-name (concatenate 'string "*** " raw-data " ***")))
+         ,@body))))
+
+(experiment-define-data-update-handler test-experiment-macro (my-model)
+  (format t "The model contains ~a" my-model))
+
+(test-experiment-macro "abcd")
